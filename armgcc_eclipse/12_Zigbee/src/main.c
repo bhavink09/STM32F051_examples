d1f41491d8f5f543ad23b3a85ec0846eaaf36dd5
Bhavin Kamdar <bhavin.kamdar.28@gmail.com>
2015-12-16 09:43:21 +0530

    Fix wsn port mode selection for armgcc examples

--- armgcc_eclipse/12_Zigbee/src/main.c
+++ armgcc_eclipse/12_Zigbee/src/main.c
@@ -59,31 +59,31 @@ int main(void)
 {	       
   char node_name[32];
   uint32_t cnt;
-  
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 	// enable the clock to GPIOA
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 	// enable the clock to GPIOB
-	RCC->AHBENR |= RCC_AHBENR_GPIOCEN; 	// enable the clock to GPIOC
 
-	// Put PORTC.8 in output mode
-	GPIOC->MODER |= (1 << 16);
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN; 	// enable the clock to GPIOA
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 	// enable the clock to GPIOB
+  RCC->AHBENR |= RCC_AHBENR_GPIOCEN; 	// enable the clock to GPIOC
 
-	// Put PORTC.9 in output mode
-	GPIOC->MODER |= (1 << 18);
+  // Put PORTC.8 in output mode
+  GPIOC->MODER |= (1 << 16);
 
-	// Put PORTA.0 in input mode
-	GPIOA->MODER &= ~(3 << 0);
-  
+  // Put PORTC.9 in output mode
+  GPIOC->MODER |= (1 << 18);
+
+  // Put PORTA.0 in input mode
+  GPIOA->MODER &= ~(3 << 0);
+
   // Make PORTC.4,.5,.6,.7 as output for select line
-  WSN_PORT->MODER |= ((1 << WSN_GPS_PIN) | (1 << WSN_WIFI_PIN) | 
-                      (1 << WSN_BT_PIN)  | (1 << WSN_ZBEE_PIN));
+  WSN_PORT->MODER |= ((1 << (WSN_GPS_PIN * 2)) | (1 << (WSN_WIFI_PIN * 2)) |
+                      (1 << (WSN_BT_PIN * 2))  | (1 << (WSN_ZBEE_PIN * 2)));
 
   // Pulldown mux pins
-  WSN_PORT->PUPDR |= ((2 << (WSN_GPS_PIN * 2)) | (2 << (WSN_WIFI_PIN * 2)) | 
-                      (2 << (WSN_BT_PIN * 2))  | (2 << (WSN_ZBEE_PIN * 2)));
+  WSN_PORT->PUPDR |= ((1 << (WSN_GPS_PIN * 2)) | (1 << (WSN_WIFI_PIN * 2)) |
+                      (1 << (WSN_BT_PIN * 2))  | (1 << (WSN_ZBEE_PIN * 2)));
                       
-	// This configures interrupt such that SysTick_Handler is called
-	// at ever TIMER_TICK_HZ i.e. 1/1000 = 1ms
-	SysTick_Config(SystemCoreClock / TIMER_TICK_HZ);
+  // This configures interrupt such that SysTick_Handler is called
+  // at ever TIMER_TICK_HZ i.e. 1/1000 = 1ms
+  SysTick_Config(SystemCoreClock / TIMER_TICK_HZ);
 	
   usart1_init();
   usart2_init();  
@@ -91,8 +91,8 @@ int main(void)
   // Select BT for usart2 on wsn channel mux
   wsn_select_zbee();
   
-	usart1_puts("STM32F051\r\n");	  
-	usart1_puts("Zigbee TEST\r\n");
+  usart1_puts("STM32F051\r\n");
+  usart1_puts("Zigbee TEST\r\n");
   
   usart1_puts("Enter Node name: ");
   
